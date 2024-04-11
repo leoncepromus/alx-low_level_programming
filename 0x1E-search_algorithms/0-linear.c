5b849df6b5bb7ddceb9cfd6f2ce63d624d15db36
leoncepromus <twayinganyiki.l@gmail.com>
2024-04-11 05:39:35 -0700

    fix 0-linear execution

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+
+    return (EXIT_SUCCESS);
+}
+
+/**
+ * linear_search - Searches for a value in an array of integers
+ * using the Linear search algorithm
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in array
+ * @value: The value to search for
+ *
+ * Return: The first index where value is located,
+ *         or -1 if value is not present in array or if array is NULL
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return -1;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return i;
+    }
+
+    return -1;
+}

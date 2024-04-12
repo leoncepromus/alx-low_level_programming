80e09f5c8c439cc3578e1adcc81ed7ec0293f493
leoncepromus <twayinganyiki.l@gmail.com>
2024-04-12 05:30:05 -0700

    include  binary .h

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,45 +1,39 @@
-#include <stdio.h>
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array of integers
- * using the Binary search algorithm
- * @array: A pointer to the first element of the array to search in
- * @size: The number of elements in array
- * @value: The value to search for
- *
- * Return: The index where value is located,
- *         or -1 if value is not present in array or if array is NULL
- */
+  * binary_search - Searches for a value in a sorted array
+  *                 of integers using binary search.
+  * @array: A pointer to the first element of the array to search.
+  * @size: The number of elements in the array.
+  * @value: The value to search for.
+  *
+  * Return: If the value is not present or the array is NULL, -1.
+  *         Otherwise, the index where the value is located.
+  *
+  * Description: Prints the [sub]array being searched after each change.
+  */
 int binary_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;
+	size_t i, left, right;
 
-    size_t left = 0;
-    size_t right = size - 1;
+	if (array == NULL)
+		return (-1);
 
-    while (left <= right)
-    {
-        // Print current array being searched
-        printf("Searching in array: ");
-        for (size_t i = left; i <= right; i++)
-        {
-            if (i > left)
-                printf(", ");
-            printf("%d", array[i]);
-        }
-        printf("\n");
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
 
-        size_t mid = left + (right - left) / 2;
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
 
-        if (array[mid] == value)
-            return mid; // Element found, return index
-        else if (array[mid] < value)
-            left = mid + 1; // Target is in the right half
-        else
-            right = mid - 1; // Target is in the left half
-    }
-
-    return -1; // Element not found
+	return (-1);
 }
